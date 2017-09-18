00756ffb13495b9140e645c004fefd8771cd1347
autodocs <autodocs>
2017-09-18 15:45:18 +0000

    build based on 4f40af2

--- latest/search_index.js
+++ latest/search_index.js
@@ -13,7 +13,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "TypedBools.TypedBool",
     "category": "Type",
-    "text": "abstract TypedBool\n\nThere are two TypedBools: True and False. They can be converted to Bools. Logical operations and ifelse are defined for them. Using TypedBools can lead to type stability in cases where constant propogation is not working for Bools.\n\njulia> using TypedBools, Base.Test\n\njulia> if_unstable(x) = Bool(x) ? 1 : \"a\";\n\njulia> ifelse_unstable(x) = ifelse(x, 1, \"a\");\n\njulia> @inferred if_unstable(!(True() & True() & False()))\n1\n\njulia> @inferred if_unstable(False() & False() & True())\n\"a\"\n\njulia> @inferred ifelse_unstable(!(True() | True() | False()))\n\"a\"\n\njulia> @inferred ifelse_unstable(False() | False() | True())\n1\n\n\n\n"
+    "text": "abstract TypedBool\n\nThere are two TypedBools: True and False. They can be converted to Bools. Logical operations and ifelse are defined for them. Using TypedBools can lead to type stability in cases where constant propogation is not working for Bools.\n\njulia> using TypedBools, Base.Test\n\njulia> if_unstable(x) = Bool(x) ? 1 : \"a\";\n\njulia> ifelse_unstable(x) = ifelse(x, 1, \"a\");\n\njulia> @inferred if_unstable(!(True() & True() & False()))\n1\n\njulia> @inferred if_unstable(False() & False() & True())\n\"a\"\n\njulia> @inferred ifelse_unstable(!(True() | True() | False()))\n\"a\"\n\njulia> @inferred ifelse_unstable(False() | False() | True())\n1\n\njulia> @inferred if_unstable(typed(true) & typed(false))\n\"a\"\n\n\n\n"
 },
 
 {
