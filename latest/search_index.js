e28d59d00e50b267e4c7a8dd0f48b3f9f906a47a
autodocs <autodocs>
2018-03-04 15:23:49 +0000

    build based on a0938d2

--- latest/search_index.js
+++ latest/search_index.js
@@ -13,7 +13,15 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "TypedBools.not",
     "category": "method",
-    "text": "not(::False) = True()\nnot(::True) = False()\n\njulia> using TypedBools\n\njulia> not(True())\nTypedBools.False()\n\njulia> not(False())\nTypedBools.True()\n\n\n\n"
+    "text": "not(x)\n\nNegate a TypedBool\n\njulia> using TypedBools\n\njulia> not(True())\nTypedBools.False()\n\njulia> not(False())\nTypedBools.True()\n\n\n\n"
+},
+
+{
+    "location": "index.html#TypedBools.typed-Tuple{Any}",
+    "page": "Home",
+    "title": "TypedBools.typed",
+    "category": "method",
+    "text": "typed(x)\n\nConvert a Bool to a TypedBool\n\njulia> using TypedBools, Base.Test\n\njulia> typed(false)\nTypedBools.False()\n\njulia> typed(true)\nTypedBools.True()\n\n\n\n"
 },
 
 {
@@ -21,7 +29,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "TypedBools.TypedBool",
     "category": "type",
-    "text": "abstract TypedBool\n\nThere are two TypedBools: True and False. They can be converted to Bools. Logical operations and ifelse are defined for them. Using TypedBools can lead to type stability in cases where constant propogation is not working for Bools.\n\njulia> using TypedBools, Base.Test\n\njulia> if_unstable(x) = Bool(x) ? 1 : \"a\";\n\njulia> ifelse_unstable(x) = ifelse(x, 1, \"a\");\n\njulia> @inferred if_unstable(not(True() & True() & False()))\n1\n\njulia> @inferred if_unstable(False() & False() & True())\n\"a\"\n\njulia> @inferred ifelse_unstable(not(True() | True() | False()))\n\"a\"\n\njulia> @inferred ifelse_unstable(False() | False() | True())\n1\n\njulia> @inferred if_unstable(typed(true) & typed(false))\n\"a\"\n\n\n\n"
+    "text": "abstract TypedBool\n\nThere are two TypedBools: True and False. They can be converted to Bools. Logical operations and ifelse are defined for them. Using TypedBools can lead to type stability in cases where constant propogation is not working for Bools.\n\njulia> using TypedBools\n\njulia> True() & True() & False()\nTypedBools.False()\n\njulia> False() & False() & True()\nTypedBools.False()\n\njulia> True() | True() | False()\nTypedBools.True()\n\njulia> False() | False() | True()\nTypedBools.True()\n\n\n\n"
 },
 
 {
